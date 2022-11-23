ffa2875c2a22a817121f4804a58132220f0b64ec
bonvy <49160177+bonvy@users.noreply.github.com>
2022-11-23 09:32:18 +0100

    Update network.py

---
+++ network.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import OVSKernelSwitch, RemoteController
+from mininet.cli import CLI
+from mininet.link import TCLink
