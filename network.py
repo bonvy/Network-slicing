c1c19ba548f9eb61602b72cf3d20d5ed672fa963
bonvy <49160177+bonvy@users.noreply.github.com>
2022-11-24 16:28:13 +0100

    Creazione rete

--- network.py
+++ network.py
@@ -5,3 +5,60 @@ from mininet.net import Mininet
 from mininet.node import OVSKernelSwitch, RemoteController
 from mininet.cli import CLI
 from mininet.link import TCLink
+
+def emptyNet():
+
+
+    net = Mininet( controller=RemoteController, switch=NullSwitch )
+
+    info( '*** Adding controller\n' )
+    net.addController( 'c0' )
+
+    host_config = dict(inNamespace=True)
+    switch_config = dict(bw=20)
+
+     #create switch nodes
+        for i in range(6):
+            sconfig = {"dpid": "%016x" % (i + 1)}
+            net.addSwitch("s%d" % (i + 1), **sconfig)
+
+        #create host nodes
+        for i in range(9):
+            net.addHost("h%d" % (i + 1), **host_config)
+
+        #network core links
+        net.addLink("s1","s2", **switch_config)
+        net.addLink("s1","s3", **switch_config)
+        net.addLink("s3","s2", **switch_config)
+
+        #peripheral links
+        net.addLink("s1","s4", **switch_config)
+        net.addLink("s2","s5", **switch_config)
+        net.addLink("s3","s6", **switch_config)
+
+        net.addLink("h1","s4", **host_config)
+        net.addLink("h2","s4", **host_config)
+        net.addLink("h3","s1", **host_config)
+
+        net.addLink("h4","s5", **host_config)
+        net.addLink("h5","s5", **host_config)
+        net.addLink("h6","s2", **host_config)
+
+        net.addLink("h7","s6", **host_config)
+        net.addLink("h8","s6", **host_config)
+        net.addLink("h9","s3", **host_config)
+
+    
+
+    info( '*** Starting network\n')
+    net.start()
+
+    info( '*** Running CLI\n' )
+    CLI( net )
+
+    info( '*** Stopping network' )
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    emptyNet()
