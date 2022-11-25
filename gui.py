5b1f50559aadb36f5ed0b4d42756aa0d62389da9
bonvy <49160177+bonvy@users.noreply.github.com>
2022-11-25 13:09:11 +0100

    prova

--- gui.py
+++ gui.py
@@ -1,22 +1,75 @@
-from tkinter import *
-from tkinter import ttk
-from mininet.net import Mininet
+import plotly.graph_objects as go
+import networkx as nx
 
-def addSW():
-    i=7
-    sconfig = {"dpid": "%016x" % (i + 1)}
-    net.addSwitch("s%d" % (i + 1), **sconfig)
-net=Mininet()
-root = Tk() #finestra
-root.title("Test")
-root.geometry("250x250")#definizione grandezza finestra
+G = nx.random_geometric_graph(200, 0.125)
+edge_x = []
+edge_y = []
+for edge in G.edges():
+    x0, y0 = G.nodes[edge[0]]['pos']
+    x1, y1 = G.nodes[edge[1]]['pos']
+    edge_x.append(x0)
+    edge_x.append(x1)
+    edge_x.append(None)
+    edge_y.append(y0)
+    edge_y.append(y1)
+    edge_y.append(None)
 
-mainframe = ttk.Frame(root, padding="3 3 12 12") #simile ad un div
-mainframe.grid(column=0, row=0, sticky=(N,W,E,S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+edge_trace = go.Scatter(
+    x=edge_x, y=edge_y,
+    line=dict(width=0.5, color='#888'),
+    hoverinfo='none',
+    mode='lines')
 
-ttk.Label(mainframe,text="ciao").grid(column=2,row=1, sticky=W)
-ttk.Button(mainframe,text="aggiungi host",command=addSW()).grid(column=2, row= 2,sticky=W)
-root.mainloop()
+node_x = []
+node_y = []
+for node in G.nodes():
+    x, y = G.nodes[node]['pos']
+    node_x.append(x)
+    node_y.append(y)
 
+node_trace = go.Scatter(
+    x=node_x, y=node_y,
+    mode='markers',
+    hoverinfo='text',
+    marker=dict(
+        showscale=True,
+        # colorscale options
+        #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
+        #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
+        #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
+        colorscale='YlGnBu',
+        reversescale=True,
+        color=[],
+        size=10,
+        colorbar=dict(
+            thickness=15,
+            title='Node Connections',
+            xanchor='left',
+            titleside='right'
+        ),
+        line_width=2))
+node_adjacencies = []
+node_text = []
+for node, adjacencies in enumerate(G.adjacency()):
+    node_adjacencies.append(len(adjacencies[1]))
+    node_text.append('# of connections: '+str(len(adjacencies[1])))
+
+node_trace.marker.color = node_adjacencies
+node_trace.text = node_text
+
+fig = go.Figure(data=[edge_trace, node_trace],
+             layout=go.Layout(
+                title='<br>Network graph made with Python',
+                titlefont_size=16,
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20,l=5,r=5,t=40),
+                annotations=[ dict(
+                    text="Python code: <a href='https://plotly.com/ipython-notebooks/network-graphs/'> https://plotly.com/ipython-notebooks/network-graphs/</a>",
+                    showarrow=False,
+                    xref="paper", yref="paper",
+                    x=0.005, y=-0.002 ) ],
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                )
+fig.show()        
