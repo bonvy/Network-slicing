53a20091366a3cf0e1186df18e217cb217ecb203
bonvy <49160177+bonvy@users.noreply.github.com>
2022-11-28 11:34:25 +0100

    hvjkd

--- test.py
+++ test.py
@@ -6,14 +6,11 @@ from ryu.ofproto import ofproto_v1_0
 from ruy.api import get_switch, get_link, get_host
 
 class test(app_manager.RyuApp):
-     OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
-     def __init__(self, *args, **kwargs):
+    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
+    def __init__(self, *args, **kwargs):
         super(test, self).__init__(*args, **kwargs)
-    
-    def ciao():
-        print("hello word")
-    
+
     def switch(self):
-        get_switch(self)
+        return get_switch(self)
 
      
