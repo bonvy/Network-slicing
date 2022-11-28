283b7adb5e9f4ad3d2ad7acd346b63dba2041bc1
bonvy <49160177+bonvy@users.noreply.github.com>
2022-11-28 13:40:12 +0100

    cx

--- excute.py
+++ excute.py
@@ -4,6 +4,8 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_0
+from ryu.base import app_manager
+from ryu.topology import event
 app = Flask(__name__)
 
 @app.route('/mio')
@@ -13,7 +15,10 @@ def tmp():
 @app.route('/')
 def hello_world():
    return '<a href="/switch">Prova</a>'
-
+@app.route('/switch')
+def get_switch(app, dpid=None):
+    rep = app.send_request(event.EventSwitchRequest(dpid))
+    return rep.switches
 
   
 
@@ -25,22 +30,9 @@ class L2Switch(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(L2Switch, self).__init__(*args, **kwargs)
         app.run()
+   
+   
+   
 
-    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def packet_in_handler(self, ev):
-        msg = ev.msg
-        dp = msg.datapath
-        ofp = dp.ofproto
-        ofp_parser = dp.ofproto_parser
-
-        actions = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD)]
-
-        data = None
-        if msg.buffer_id == ofp.OFP_NO_BUFFER:
-             data = msg.data
-
-        out = ofp_parser.OFPPacketOut(
-            datapath=dp, buffer_id=msg.buffer_id, in_port=msg.in_port,
-            actions=actions, data = data)
-        dp.send_msg(out)
+      
 
