696eacf41606f57fa5969e15f3974d95254764c2
bonvy <49160177+bonvy@users.noreply.github.com>
2022-12-01 11:15:18 +0100

    git

--- html/ryu.topology.js
+++ html/ryu.topology.js
@@ -75,7 +75,7 @@ elem.link = elem.svg.selectAll(".link");
 elem.port = elem.svg.selectAll(".port");
 elem.update = function () {
     this.force
-        .swithces(topo.swithces)
+        .nodes(topo.nodes)
         .links(topo.links)
         .start();
 
@@ -84,7 +84,7 @@ elem.update = function () {
     this.link.enter().append("line")
         .attr("class", "link");
 
-    this.node = this.node.data(topo.swithces);
+    this.node = this.node.data(topo.nodes);
     this.node.exit().remove();
     var nodeEnter = this.node.enter().append("g")
         .attr("class", "node")
@@ -119,25 +119,17 @@ function is_valid_link(link) {
 }
 
 var topo = {
-    swithces: [],
+    nodes: [],
     links: [],
-    hosts: [],
-    node_index: {}, // dpid -> index of swithces array
+    node_index: {}, // dpid -> index of nodes array
     initialize: function (data) {
         console.log(data)
-        this.add_switches(data.switches);
+        this.add_nodes(data.switches);
         this.add_links(data.links);
-        this.add_hosts(data.hosts)
     },
-    add_hosts: function(hosts){
-        for (var i = 0; i < hosts.length; i++) {
-            this.swithces.push(hosts[i]);
-        }
-        //this.refresh_node_index();
-    },
-    add_switches: function (swithces) {
-        for (var i = 0; i < swithces.length; i++) {
-            this.swithces.push(swithces[i]);
+    add_nodes: function (nodes) {
+        for (var i = 0; i < nodes.length; i++) {
+            this.nodes.push(nodes[i]);
         }
         this.refresh_node_index();
     },
@@ -161,12 +153,12 @@ var topo = {
             this.links.push(link);
         }
     },
-    delete_nodes: function (swithces) {
-        for (var i = 0; i < swithces.length; i++) {
-            console.log("delete switch: " + JSON.stringify(swithces[i]));
+    delete_nodes: function (nodes) {
+        for (var i = 0; i < nodes.length; i++) {
+            console.log("delete switch: " + JSON.stringify(nodes[i]));
 
-            node_index = this.get_node_index(swithces[i]);
-            this.swithces.splice(node_index, 1);
+            node_index = this.get_node_index(nodes[i]);
+            this.nodes.splice(node_index, 1);
         }
         this.refresh_node_index();
     },
@@ -180,8 +172,8 @@ var topo = {
         }
     },
     get_node_index: function (node) {
-        for (var i = 0; i < this.swithces.length; i++) {
-            if (node.dpid == this.swithces[i].dpid) {
+        for (var i = 0; i < this.nodes.length; i++) {
+            if (node.dpid == this.nodes[i].dpid) {
                 return i;
             }
         }
@@ -237,8 +229,8 @@ var topo = {
     },
     refresh_node_index: function(){
         this.node_index = {};
-        for (var i = 0; i < this.swithces.length; i++) {
-            this.node_index[this.swithces[i].dpid] = i;
+        for (var i = 0; i < this.nodes.length; i++) {
+            this.node_index[this.nodes[i].dpid] = i;
         }
     },
 }
@@ -249,7 +241,7 @@ var rpc = {
         for(var i=0; i < params.length; i++){
             switches.push({"dpid":params[i].dpid,"ports":params[i].ports});
         }
-        topo.add_switches(switches);
+        topo.add_nodes(switches);
         elem.update();
         return "";
     },
@@ -275,14 +267,15 @@ var rpc = {
 }
 
 function initialize_topology() {
-    d3.json("/v1.0/topology/switches", function(error, switches) {
-        //d3.json("/v1.0/topology/links", function(error, links) {
-            d3.json("/v1.0/topology/hosts", function(error, hosts){
-                topo.initialize({switches: switches,  hosts: hosts});
+    d3.json("/v1.0/topology/hosts", function(error, switches) {
+        d3.json("/v1.0/topology/links", function(error, links) {
+            d3.json("v1.0/topology/hosts", function(error, hosts){
+                topo.initialize({switches: switches, links: links, hosts: hosts});
+                
             elem.update();
             });
             
-        //});
+        });
     });
 }
 
