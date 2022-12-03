184bb81ccdd71368020366b6620f108e1280c816
bonvy <49160177+bonvy@users.noreply.github.com>
2022-12-03 18:39:36 +0100

    Update gui_start.py

--- gui_start.py
+++ gui_start.py
@@ -44,4 +44,3 @@ class GUIServerController(ControllerBase):
 app_manager.require_app('ryu.app.rest_topology')
 app_manager.require_app('ryu.app.ws_topology')
 app_manager.require_app('ryu.app.ofctl_rest')
-app_manager.require_app('rest_host')
