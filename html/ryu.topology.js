10169bda3a83e226cdc7947289d08bbe5d4a7a1b
bonvy <49160177+bonvy@users.noreply.github.com>
2022-12-03 18:04:26 +0100

    Update ryu.topology.js

--- html/ryu.topology.js
+++ html/ryu.topology.js
@@ -270,7 +270,7 @@ function initialize_topology() {
     d3.json("/v1.0/topology/switches", function(error, switches) {
         d3.json("/v1.0/topology/links", function(error, links) {
            
-            topo.initialize({switches: switches, links: links, hosts: hosts});
+            topo.initialize({switches: switches, links: links});
             elem.update();
          
            
