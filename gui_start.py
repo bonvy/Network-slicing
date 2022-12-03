f8891f7659c8cd8a8851411865b6d9aa29772231
bonvy <49160177+bonvy@users.noreply.github.com>
2022-12-03 16:59:17 +0100

    pro

--- gui_start.py
+++ gui_start.py
@@ -1,10 +1,11 @@
 import os
 
 from webob.static import DirectoryApp
-
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.app.wsgi import ControllerBase, WSGIApplication, route
+from ryu.controller.handler import set_ev_cls
 from ryu.base import app_manager
-
+from ryu.topology import event
 
 PATH = os.path.dirname(__file__)
 
@@ -23,6 +24,7 @@ class GUIServerApp(app_manager.RyuApp):
 
 
 class GUIServerController(ControllerBase):
+    i=0
     def __init__(self, req, link, data, **config):
         super(GUIServerController, self).__init__(req, link, data, **config)
         path = "%s/html/" % PATH
@@ -33,6 +35,12 @@ class GUIServerController(ControllerBase):
         if kwargs['filename']:
             req.path_info = kwargs['filename']
         return self.static_app(req)
+
+    @set_ev_cls(event.EventHostAdd, MAIN_DISPATCHER)
+    def NewHost(self, ev):
+        i+1
+        print(i)
+    
 
 
 app_manager.require_app('ryu.app.rest_topology')
