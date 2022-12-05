f002ab61d59cf1bfaba8e9ecb942b69461aaee3e
bonvy <49160177+bonvy@users.noreply.github.com>
2022-12-05 15:35:13 +0100

    v

--- html/ryu.topology.js
+++ html/ryu.topology.js
@@ -85,7 +85,7 @@ elem.update = function () {
 
     this.node = this.node.data(topo.nodes);
     this.node.exit().remove();
-    console.log(this.node)
+    console.log(this.node[0])
     var nodeEnter = this.node.enter().append("g")
         .attr("class", "node")
         .on("dblclick", function(d) { d3.select(this).classed("fixed", d.fixed = false); })
