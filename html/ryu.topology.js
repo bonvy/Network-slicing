ae9733d5e610e679efb44658abc6855d2c3ca9ff
bonvy <49160177+bonvy@users.noreply.github.com>
2022-12-06 17:39:26 +0100

    d

--- html/ryu.topology.js
+++ html/ryu.topology.js
@@ -175,7 +175,7 @@ var topo = {
             
             this.nodes[t]=hosts[i]
             
-            this.add_Host_Sw_link(t,hosts[i].port.dpid)
+            this.add_Host_Sw_link(t,hosts[i])
             t++;
         }
         this.refresh_node_index();
