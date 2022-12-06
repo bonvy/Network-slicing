c10f38e78d7e0b10320438cf2392b0b423741167
bonvy <49160177+bonvy@users.noreply.github.com>
2022-12-06 17:33:56 +0100

    d

--- html/ryu.topology.js
+++ html/ryu.topology.js
@@ -148,7 +148,7 @@ var topo = {
         this.add_nodes(data.switches);
         this.add_links(data.links);
         this.add_hosts(data.hosts,this.nodes);
-        console.log(data)
+        console.log(links)
         
     },
     add_nodes: function (nodes) {
