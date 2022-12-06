ace9ca475e1ad62a237c5f5d18802e856d017804
bonvy <49160177+bonvy@users.noreply.github.com>
2022-12-06 17:14:30 +0100

    c

--- html/ryu.topology.js
+++ html/ryu.topology.js
@@ -61,7 +61,7 @@ var elem = {
         .attr("width", CONF.force.width)
 };
 function _tick() {
-    elem.link.attr("x1", function(d) { return d.source.x; console.log(d.source.x) })
+    elem.link.attr("x1", function(d) {  console.log(d.source.x); return d.source.x;  })
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
