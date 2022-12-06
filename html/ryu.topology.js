a46250bcae7d63c3a68773594e61546642eec2d9
bonvy <49160177+bonvy@users.noreply.github.com>
2022-12-06 09:05:58 +0100

    v

--- html/ryu.topology.js
+++ html/ryu.topology.js
@@ -94,7 +94,7 @@ elem.update = function () {
         .call(this.drag);
     
     tmp = nodeEnter[0]
-    console.log(tmp[0].__data__.dpid)
+    console.log(tmp[0].__data__)
     tmp.forEach(element => console.log("ciao"))
     nodeEnter.append("image")
         .attr("xlink:href", "./router.svg")
