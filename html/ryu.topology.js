acf05bba8ee090d4fdbe8f52514dc8d52754594d
bonvy <49160177+bonvy@users.noreply.github.com>
2022-12-06 14:47:14 +0100

    Update ryu.topology.js

--- html/ryu.topology.js
+++ html/ryu.topology.js
@@ -98,18 +98,18 @@ elem.update = function () {
     tmp.forEach(function(element){
             if(element.__data__.dpid!=null){
                 console.log("ehi")
-                
+                element.append("image")
+                    .attr("xlink:href", "./router.svg")
+                    .attr("x", -CONF.image.width/2)
+                    .attr("y", -CONF.image.height/2)
+                    .attr("width", CONF.image.width)
+                    .attr("height", CONF.image.height);
       
             }else{
                 console.log("buu")
             }
     })
-    nodeEnter.append("image")
-        .attr("xlink:href", "./router.svg")
-        .attr("x", -CONF.image.width/2)
-        .attr("y", -CONF.image.height/2)
-        .attr("width", CONF.image.width)
-        .attr("height", CONF.image.height);
+    
     nodeEnter.append("text")
         .attr("dx", -CONF.image.width/2)
         .attr("dy", CONF.image.height-10)
