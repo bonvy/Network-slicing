44120e407ba52a4a9c04963047a2a1c1dd23adbc
bonvy <49160177+bonvy@users.noreply.github.com>
2022-12-18 10:47:22 +0100

    Update ryu.topology.js

--- html/ryu.topology.js
+++ html/ryu.topology.js
@@ -56,7 +56,7 @@ var elem = {
         .attr("id", "topology")
         .attr("width", CONF.force.width)
         .attr("height", CONF.force.height),
-    button: d3.select("body").append(document.createElement("button").innerHTML="Save"),
+    button: d3.select("body").append(document.createElement("button").innerText="Save"),
     console: d3.select("body").append("div")
         .attr("id", "console")
         .attr("width", CONF.force.width)
